269ac47b19065c0af5542937067371ab6bfe59d2
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-12-07 11:57:36 +0800

    优化模型

--- course3/model.py
+++ course3/model.py
@@ -8,8 +8,8 @@ LOG_SIG_MIN = -20.0
 class ActorModel(parl.Model):
     def __init__(self, act_dim):
         self.conv1 = layers.conv2d(num_filters=32, filter_size=3, stride=1, padding=0, act='relu')
-        self.conv2 = layers.conv2d(num_filters=64, filter_size=3, stride=1, padding=0, act='relu')
-        self.conv3 = layers.conv2d(num_filters=64, filter_size=3, stride=1, padding=0, act='relu')
+        self.conv2 = layers.conv2d(num_filters=32, filter_size=3, stride=1, padding=0, act='relu')
+        self.conv3 = layers.conv2d(num_filters=32, filter_size=3, stride=1, padding=0, act='relu')
 
         self.fc1 = layers.fc(size=512, act='relu')
         self.fc2 = layers.fc(size=512, act='relu')
@@ -17,7 +17,13 @@ class ActorModel(parl.Model):
         self.log_std_linear = layers.fc(size=act_dim)
 
     def policy(self, obs):
-        hid1 = self.fc1(obs)
+        conv1 = self.conv1(obs)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+
+        pool = layers.pool2d(input=conv3, pool_size=2, pool_stride=2)
+
+        hid1 = self.fc1(pool)
         hid2 = self.fc2(hid1)
         means = self.mean_linear(hid2)
         log_std = self.log_std_linear(hid2)
@@ -45,7 +51,9 @@ class CriticModel(parl.Model):
         conv2 = self.conv2(conv1)
         conv3 = self.conv3(conv2)
 
-        hid1 = self.fc1(conv3)
+        pool1 = layers.pool2d(input=conv3, pool_size=2, pool_stride=2)
+
+        hid1 = self.fc1(pool1)
         concat1 = layers.concat([hid1, act], axis=1)
         Q1 = self.fc2(concat1)
         Q1 = self.fc3(Q1)
@@ -55,7 +63,9 @@ class CriticModel(parl.Model):
         conv5 = self.conv2(conv4)
         conv6 = self.conv3(conv5)
 
-        hid2 = self.fc4(conv6)
+        pool2 = layers.pool2d(input=conv6, pool_size=2, pool_stride=2)
+
+        hid2 = self.fc4(pool2)
         concat2 = layers.concat([hid2, act], axis=1)
         Q2 = self.fc5(concat2)
         Q2 = self.fc6(Q2)
