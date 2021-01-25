681a9a9782cc8f5c2299b82d7ab3368fd7d8f532
yeyupiaoling <yeyupiaoling@foxmail.com>
2021-01-25 17:18:26 +0800

    添加手写动态版本飞翔的小鸟

--- course2-1/train.py
+++ course2-1/train.py
@@ -1,23 +1,30 @@
-import gym
+import os
+
+import cv2
 import numpy as np
 import paddle
+
+import flappy_bird.wrapped_flappy_bird as flappyBird
 from model import Model
 from replay_memory import ReplayMemory
 
 # 定义训练的参数
 batch_size = 64  # batch大小
 num_episodes = 10000  # 训练次数
-memory_size = 10000  # 内存记忆
+memory_size = 20000  # 内存记忆
 learning_rate = 1e-3  # 学习率大小
-gamma = 1.0  # 奖励系数
 e_greed = 0.1  # 探索初始概率
+gamma = 0.99  # 奖励系数
 e_greed_decrement = 1e-6  # 在训练过程中，降低探索的概率
 update_num = 0  # 用于计算目标模型更新次数
+resize_shape = (1, 224, 224)  # 训练缩放的大小，减少模型计算，原大小（288, 512）
+save_model_path = "models/model.ckpt"  # 保存模型路径
 
 # 实例化一个游戏环境，参数为游戏名称
-env = gym.make("CartPole-v1")
-obs_dim = env.observation_space.shape[0]
-action_dim = env.action_space.n
+env = flappyBird.GameState()
+# 图像输入形状和动作维度
+obs_dim = resize_shape[0]
+action_dim = env.action_dim
 
 # 创建策略模型和目标模型，目标模型不参与训练
 policyQ = Model(obs_dim, action_dim)
@@ -31,16 +38,36 @@ optimizer = paddle.optimizer.Adam(parameters=policyQ.parameters(),
                                   learning_rate=learning_rate)
 
 
+# 图像预处理
+def preprocess(observation):
+    # 裁剪图像
+    observation = observation[:observation.shape[0] - 100, :]
+    # 缩放图像
+    observation = cv2.resize(observation, (resize_shape[1], resize_shape[2]))
+    # 把图像转成灰度图
+    observation = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
+    # 图像转换成非黑即白的图像
+    ret, observation = cv2.threshold(observation, 1, 255, cv2.THRESH_BINARY)
+    # 显示处理过的图像
+    cv2.imshow("preprocess", observation)
+    cv2.waitKey(1)
+    observation = np.expand_dims(observation, axis=0)
+    observation = observation / 255.0
+    return observation
+
+
 # 评估模型
 def evaluate():
     total_reward = 0
-    state = env.reset()
+    obs = env.reset()
     while True:
-        state = paddle.to_tensor(state, dtype='float32')
-        action = targetQ(state)
+        obs = preprocess(obs)
+        obs = np.expand_dims(obs, axis=0)
+        obs = paddle.to_tensor(obs, dtype='float32')
+        action = targetQ(obs)
         action = paddle.argmax(action).numpy()[0]
-        next_state, reward, done, info = env.step(action)
-        state = next_state
+        next_obs, reward, done, info = env.step(action)
+        obs = next_obs
         total_reward += reward
 
         if done:
@@ -53,40 +80,41 @@ def train():
     global e_greed, update_num
     total_reward = 0
     # 重置游戏状态
-    state = env.reset()
+    obs = env.reset()
+    obs = preprocess(obs)
 
     while True:
-        # 显示游戏图像
-        env.render()
         # 使用贪心算法获取游戏动作的来源
         e_greed = max(0.01, e_greed - e_greed_decrement)
         if np.random.rand() < e_greed:
             # 随机生成动作
-            action = env.action_space.sample()
+            action = env.action_space()
         else:
             # 策略模型预测游戏动作
-            action = policyQ(paddle.to_tensor(state, dtype='float32'))
+            obs1 = np.expand_dims(obs, axis=0)
+            action = policyQ(paddle.to_tensor(obs1, dtype='float32'))
             action = paddle.argmax(action).numpy()[0]
 
         # 执行游戏
-        next_state, reward, done, info = env.step(action)
+        next_obs, reward, done, info = env.step(action)
+        next_obs = preprocess(next_obs)
         total_reward += reward
         # 记录游戏数据
-        rpm.append((state, action, reward, next_state, done))
-        state = next_state
+        rpm.append((obs, action, reward, next_obs, done))
+        obs = next_obs
         # 游戏结束
         if done:
             break
         # 记录的数据打印batch_size就开始训练
         if rpm.__len__() > batch_size:
             # 获取训练数据
-            batch_state, batch_action, batch_reword, batch_next_state, batch_done = rpm.sample(batch_size)
+            batch_obs, batch_action, batch_reword, batch_next_obs, batch_done = rpm.sample(batch_size)
             # 计算损失函数
-            action_value = policyQ(batch_state)
+            action_value = policyQ(batch_obs)
             action_onehot = paddle.nn.functional.one_hot(batch_action, 2)
             pred_action_value = paddle.sum(action_value * action_onehot, axis=1)
 
-            best_v = targetQ(batch_next_state)
+            best_v = targetQ(batch_next_obs)
             best_v = paddle.max(best_v, axis=1)
 
             best_v.stop_gradient = False
@@ -114,3 +142,7 @@ if __name__ == '__main__':
 
         eval_reward = evaluate()
         print('Episode:{}    test_reward:{}'.format(episode, eval_reward))
+        # 保存模型
+        if not os.path.exists(os.path.dirname(save_model_path)):
+            os.makedirs(os.path.dirname(save_model_path))
+        paddle.save(targetQ.state_dict(), save_model_path)
